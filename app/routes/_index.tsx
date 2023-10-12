2c74595847f7e596c53906c270b94e4ea2f9dcf8
Ankur Taxali <ataxali@gmail.com>
2023-10-12 00:13:09 -0400

    Implement dynamic text input creation and focus management on user click

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from '@remix-run/node';
-import React from 'react';
+import React, { useRef } from 'react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,18 +10,49 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   const [prompts, setPrompts] = React.useState<{ x: number; y: number }[]>([]);
-  function openPromptBoxAtLocation(x, y) {
+  const [mode, setMode] = React.useState<'view' | 'edit'>('view');
+  const promptRefs = useRef<(React.RefObject<HTMLInputElement> | null)[]>([]);
+  function openPromptBoxAtLocation(x: number, y: number) {
     const newPrompt = {
       x,
       y,
     };
-    setPrompts((prompts) => [...prompts, newPrompt]);
+    setPrompts((prevPrompts) => {
+      const newPrompts = [...prevPrompts, newPrompt];
+      promptRefs.current = new Array(newPrompts.length)
+        .fill(null)
+        .map((_, i) => promptRefs.current[i] || React.createRef());
+      return newPrompts;
+    });
   }
   console.log({ prompts });
   function handleClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if (mode === 'edit') return;
     const x = e.clientX;
     const y = e.clientY;
     openPromptBoxAtLocation(x, y);
+    setMode('edit');
+    const lastRef = promptRefs.current[promptRefs.current.length - 1];
+    if (lastRef) {
+      lastRef.current?.focus();
+    }
   }
-  return <div onClick={handleClick} className='universe' id='universe'></div>;
+  console.log({ promptRefs });
+  return (
+    <div onClick={handleClick} className='universe' id='universe'>
+      {prompts &&
+        prompts.map((prompt, index) => (
+          <div
+            key={index}
+            style={{
+              position: 'absolute',
+              left: `${prompt.x}px`,
+              top: `${prompt.y}px`,
+            }}
+          >
+            <input type='text' autoFocus />
+          </div>
+        ))}
+    </div>
+  );
 }
